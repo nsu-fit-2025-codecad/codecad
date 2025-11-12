61b749179bdf069e0bc3a1277057179fe39b76bf
streafy <streafyhs@gmail.com>
2025-11-13 04:07:27 +0700

    add drawing makerjs models using monaco editor

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,11 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import { Editor } from '@monaco-editor/react';
 import { editor } from 'monaco-editor';
 import IStandaloneCodeEditor = editor.IStandaloneCodeEditor;
+import makerjs from 'makerjs';
 
 export const HomePage = () => {
+  const [svg, setSvg] = useState<string>('');
   const editorRef = useRef<IStandaloneCodeEditor | null>(null);
 
   const handleEditorDidMount = (editor: IStandaloneCodeEditor) => {
@@ -15,18 +17,53 @@ export const HomePage = () => {
     if (!value) {
       return;
     }
-    eval(value);
+    try {
+      const createModel = new Function(
+        'makerjs',
+        `return (function() {
+          ${value}
+        })();`
+      );
+
+      const model = createModel(makerjs);
+
+      const svgString = makerjs.exporter.toSVG(model);
+      setSvg(svgString);
+    } catch (error) {
+      console.error('Error:', error);
+      alert(`Error: ${(error as Error).message}`);
+    }
   };
+
+  const defaultCode = `const square = new makerjs.models.Square(100);
+
+const circle = new makerjs.models.Ring(50);
+circle.origin = [150, 0];
+
+const model = {
+  models: {
+    square: square,
+    circle: circle
+  }
+};
+
+return model;`;
 
   return (
     <div className="flex flex-col gap-1">
-      <h1>Editor</h1>
+      <h1>HomePage</h1>
+      <h2>Result Model</h2>
+      <div
+        dangerouslySetInnerHTML={{ __html: svg }}
+        className="w-full h-full"
+      />
+      <h2>Editor</h2>
       <Editor
         className="border-black border"
         height="30vh"
         width="40vw"
         defaultLanguage="javascript"
-        defaultValue="// some comment"
+        defaultValue={defaultCode}
         onMount={handleEditorDidMount}
       />
       <button
