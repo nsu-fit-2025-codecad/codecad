651f1ea652137baeff67f66b684618a495bcf805
streafy <streafyhs@gmail.com>
2025-12-13 20:11:32 +0700

    make parameters accessible directly via their names (without params.name)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -15,23 +15,15 @@ export const HomePage = () => {
       return;
     }
     try {
-      const params = parameters.reduce(
-        (acc, param) => {
-          acc[param.name] = param.value;
-          return acc;
-        },
-        {} as Record<string, number>
-      );
-
       const createModel = new Function(
         'makerjs',
-        'params',
+        ...parameters.map((p) => p.name),
         `return (function() {
           ${code}
         })();`
       );
 
-      const model = createModel(makerjs, params);
+      const model = createModel(makerjs, ...parameters.map((p) => p.value));
 
       const svgString = makerjs.exporter.toSVG(model);
 
