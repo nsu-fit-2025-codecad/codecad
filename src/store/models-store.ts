414a599d6e63536e53d676764e386060129a66f2
streafy <streafyhs@gmail.com>
2026-02-18 18:59:02 +0700

    fix(export): clean up DXF export follow-up lint issues

--- src/store/models-store.ts
+++ src/store/models-store.ts
@@ -1,5 +1,4 @@
 import { create } from 'zustand';
-import { IModel } from "makerjs";
 
 export interface Model {
   id: string;
@@ -10,16 +9,12 @@ export interface Model {
 
 interface ModelsState {
   models: Model[];
-  finalModel?: IModel;
   update: (models: Model[]) => void;
   updateFitStatus: (packedIds: Set<string>, notFitIds: Set<string>) => void;
-  setFinalModel: (model: IModel) => void;
 }
 
 export const useModelsStore = create<ModelsState>()((set) => ({
   models: [],
-  finalModel: undefined,
-
   update: (models) => set((state) => ({ ...state, models: models })),
   updateFitStatus: (packedIds, notFitIds) =>
     set((state) => ({
@@ -33,14 +28,4 @@ export const useModelsStore = create<ModelsState>()((set) => ({
         return model;
       }),
     })),
-
-    setFinalModel: (model) => set({ finalModel: model }),
 }));
-
-export interface Model {
-  id: string;
-  width: number;
-  height: number;
-  fit?: boolean;
-  makerModel?: IModel;
-}
